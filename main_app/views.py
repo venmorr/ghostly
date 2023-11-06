0212578c0ac434e32e25c80c487d54894306cfbe
venmorr <stevenmorrrca@email.com>
2023-11-06 18:21:18 -0500

    connect neon to app

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Ghost
 
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def ghost_index(request):
+  ghosts = Ghost.objects.all()
   return render(request, 'ghosts/index.html', { 'ghosts': ghosts })
 
-class Ghost:  
-  def __init__(self, name, level, type, description):
-    self.name = name
-    self.level = level
-    self.type = type
-    self.description = description
-
 ghosts = [
-  Ghost('Lolo', 1, 'tabby', 'Kinda rude.'),
-  Ghost('Sachi', 1, 'tortoiseshell', 'Looks like a turtle.'),
-  Ghost('Fancy', 1, 'bombay', 'Happy fluff ball.'),
+  Ghost('Lolo', 2, 'tabby', 'Kinda rude.'),
+  Ghost('Sachi', 3, 'tortoiseshell', 'Looks like a turtle.'),
+  Ghost('Fancy', 4, 'bombay', 'Happy fluff ball.'),
   Ghost('Bonk', 1, 'selkirk rex', 'Meows loudly.')
 ]
