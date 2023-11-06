5e0106397d61100f2c3a4f09f891bb8866b25e9d
venmorr <stevenmorrrca@email.com>
2023-11-06 16:00:04 -0500

    add index

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('ghosts/', views.ghost_index, name='ghost-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,20 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def ghost_index(request):
+  return render(request, 'ghosts/index.html', { 'ghosts': ghosts })
+
+class Ghost:  
+  def __init__(self, name, level, type, description):
+    self.name = name
+    self.level = level
+    self.type = type
+    self.description = description
+
+ghosts = [
+  Ghost('Lolo', 1, 'tabby', 'Kinda rude.'),
+  Ghost('Sachi', 1, 'tortoiseshell', 'Looks like a turtle.'),
+  Ghost('Fancy', 1, 'bombay', 'Happy fluff ball.'),
+  Ghost('Bonk', 1, 'selkirk rex', 'Meows loudly.')
+]
