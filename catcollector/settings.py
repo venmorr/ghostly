1290253b0f81751a6c70d4152ef7b3c909f6f41a
venmorr <stevenmorrrca@email.com>
2023-11-06 20:24:31 -0500

    fix authentication routing

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'ghost-index'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
